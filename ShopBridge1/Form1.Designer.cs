f6273c957e62bf248bbd83a41b63a36975a2360f
dabha <dabha@DESKTOP-PFEN8KM>
2022-03-13 15:34:13 +0530

    Add project files.

---
+++ ShopBridge1/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace ShopBridge1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(23, 108);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Add to Inventory";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Item Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Item Description";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(27, 79);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Item Price";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Location = new System.Drawing.Point(156, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 10;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(156, 45);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 11;
+           // this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(156, 76);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 12;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(26, 155);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(736, 243);
+            this.panel1.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "ShopBridge";
+            //this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ ShopBridge1/Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace ShopBridge1
+{
+    public partial class Form1 : Form
+    {
+
+        PictureBox pictureBox1;
+        PictureBox pic;
+        TextBox label4 = new TextBox();
+        Label label5 = new Label();
+        string v = null;
+        int parsedValue=0;
+        string m = null;
+
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1!=null)
+            {
+                pictureBox1 = new PictureBox();
+                pictureBox1.Location = new System.Drawing.Point(79, 40);
+                pictureBox1.Name = textBox1.Text;
+                pictureBox1.Size = new System.Drawing.Size(250, 1000);
+                pictureBox1.BackColor = Color.White;
+                pictureBox1.Image = Image.FromFile(@"E:\images\no-photo.png");
+
+                panel1.Controls.Add(pictureBox1);
+
+
+                label4.Parent = pictureBox1;
+                label4.Text = ($"Name: { v}") + "\n " + ($"Price: {m}");
+                label4.TextAlign = HorizontalAlignment.Center;//(ContentAlignment)4;
+
+                Button button = new Button();
+                button.Parent = pictureBox1;
+                button.Text = "Delete";
+                button.Location = new Point(150, 170);
+                pictureBox1.Controls.Add(button);
+
+                Button butto = new Button();
+                butto.Parent = pictureBox1;
+                butto.Text = "View";
+                butto.Location = new Point(10, 170);
+                pictureBox1.Controls.Add(butto);
+
+
+                button.Click += new EventHandler(this.button_Click);
+                butto.Click += new EventHandler(this.butto_Click);
+
+                //textBox1.Clear();
+                //textBox3.Clear();
+                button1.Click += new EventHandler(this.button1_Click);
+
+            }
+            else
+            {
+                pictureBox1 = new PictureBox();
+            }
+
+        }
+
+        
+    void button_Click(object sender, System.EventArgs e)
+        {
+            pictureBox1.Dispose();
+            pic.Dispose();
+        }
+        void butto_Click(object sender, System.EventArgs e)
+        {
+            pic = new PictureBox();
+            pic.Image = new Bitmap(@"E:\images\no-photo.png");
+            pic.Location = new System.Drawing.Point(400, -250);
+            pic.Name = textBox1.Text;
+            pic.Size = new System.Drawing.Size(250, 1000);
+            pic.BackColor = Color.White;
+            panel1.Controls.Add(pic);
+
+        }
+
+
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        { 
+            v = textBox1.Text;
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            string textboxValue = textBox3.Text;
+            int i;
+            if (!int.TryParse(textBox3.Text, out i))
+            {
+                
+                m = i.ToString();
+                try
+                {
+                    if (m == "0")
+                    {
+                        throw new InvalidOperationException(e.ToString());
+                    }
+                    
+                }
+                catch (InvalidOperationException)
+                {
+                    MessageBox.Show("Invalid input for price");
+                }
+            }
+            
+            else if (string.IsNullOrEmpty(textboxValue))
+            
+            {
+                
+            }
+            else
+            {
+                m = textBox3.Text;
+            }
+        }
+
+        
+    }
+}
+
